f0edfa83ac998524d63d89ac00f2be777c4070f5
ngoloc2k4 <ngoloc2k4@users.noreply.github.com>
2026-08-30 18:18:58 +0700

    fix(theme): add missing static import for THEME_MODE in DatastoreRepository

--- app/src/main/java/ca/ilianokokoro/umihi/music/data/repositories/DatastoreRepository.kt
+++ app/src/main/java/ca/ilianokokoro/umihi/music/data/repositories/DatastoreRepository.kt
@@ -27,6 +27,7 @@ import ca.ilianokokoro.umihi.music.data.repositories.DatastoreRepository.Prefere
 import ca.ilianokokoro.umihi.music.data.repositories.DatastoreRepository.PreferenceKeys.THUMBNAIL_CACHE_SIZE
 import ca.ilianokokoro.umihi.music.data.repositories.DatastoreRepository.PreferenceKeys.APP_VOLUME
 import ca.ilianokokoro.umihi.music.data.repositories.DatastoreRepository.PreferenceKeys.INFINITE_PLAYLIST_SUGGESTIONS
+import ca.ilianokokoro.umihi.music.data.repositories.DatastoreRepository.PreferenceKeys.THEME_MODE
 import ca.ilianokokoro.umihi.music.models.Cookies
 import ca.ilianokokoro.umihi.music.models.ThemeMode
 import ca.ilianokokoro.umihi.music.models.UmihiSettings
